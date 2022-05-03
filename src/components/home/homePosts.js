7a3a2d55fc4b0e1e7c16005a9e8b76f578d65f39
ferlobo1985 <fernando.lobo.prez@gmail.com>
2022-05-03 19:37:59 -0300

    06-Showing home posts

--- src/components/home/homePosts.js
+++ src/components/home/homePosts.js
@@ -1,20 +1,57 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
+import Masonry from 'react-masonry-css'
+import Moment from 'react-moment';
 
 import { fetchPosts } from '../../store/utils/thunks'
 
+import { Button, Spinner} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+
 
 const HomePosts = () => {
+    const homePosts = useSelector((state)=> state.posts)
     const dispatch = useDispatch();
 
 
     useEffect(()=>{
-        dispatch(fetchPosts({page:1,order:"desc",limit:6}))
+        if(homePosts.articles.items.length <= 0 ){
+            dispatch(fetchPosts({page:1,order:"desc",limit:6}))
+        }
     },[])
 
     return(
         <>
-            home posts
+            <Masonry
+                breakpointCols={{default:3,800:2,400:1}}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+            >
+                { homePosts.articles ?
+                    homePosts.articles.items.map((item)=>(
+                        <div key={item.id}>
+                            <img
+                                style={{ width:'100%',height:'200px'}}
+                                src={`${item.image}?${item.id}`}
+                                alt="some pic"
+                            />
+                            <div className='author'>
+                                <span>{item.author} -</span>
+                                <Moment format='DD MMMM'>{item.createdAt}</Moment>
+                            </div>
+                            <div className='content'>
+                                <div className='title'>{item.title}</div>
+                                <div className='excerpt'>{item.excerpt}</div>
+                                <LinkContainer to={`/article/${item.id}`} className="mt-3">
+                                    <Button variant='light'>Read more</Button>
+                                </LinkContainer>
+                            </div>
+                        </div>
+                    ))
+                :null}
+                
+            </Masonry>            
         </>
     )
 }
